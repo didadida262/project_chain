a6d6f5b805aa62eb2bee872e5655e52bd3f5f1f5
didadida262 <719713496@qq.com>
2024-08-01 10:13:25 +0800

    project_chain_init

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+/*
+ * @Description: 
+ * @Author: didadida262
+ * @Date: 2024-07-25 01:16:22
+ * @LastEditors: didadida262
+ * @LastEditTime: 2024-08-01 10:12:36
+ */
+import { useTranslation, switchLanguage } from '@/i18n';
+import ButtonTheme from '@/components/Theme/ButtonTheme'
+
+import { ButtonCommon, EButtonType} from './components/ButtonCommon';
+
+function App() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center text-green-500 bg-bgPrimaryColor">
+      Chain test
+      {/* <ButtonCommon 
+        type={EButtonType.SIMPLE}
+        onClick={() => {
+          const lan = localStorage.getItem('language')
+          switchLanguage(lan === 'zh'? 'en-US': 'zh')
+        }}
+        >
+          <span>切换语言</span>
+        </ButtonCommon>
+      <ButtonTheme/> */}
+    </div>
+  );
+}
+
+export default App;
